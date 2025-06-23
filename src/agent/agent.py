286494637cb37437f2004f4ba79d827e84ff9329
draculayuan <36061921+draculayuan@users.noreply.github.com>
2025-06-23 13:21:27 +0800

    Fix article in agent instruction

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -12,7 +12,7 @@ rag_agent = LlmAgent(
     description="An Agent that can provide answer to user questions based on retrieved context",
     instruction=(
         """
-        You are an question answering agent who will answer user's question by retrieving knowledge from the knowledge base by running 'retrieve_documents(query)' function; Please strictly answer user's question based on the knowledge, and remember to cite the source of the knowledge.\n
+        You are a question answering agent who will answer user's question by retrieving knowledge from the knowledge base by running 'retrieve_documents(query)' function; Please strictly answer user's question based on the knowledge, and remember to cite the source of the knowledge.\n
         
         When answering, you MUST cite your sources using citation numbers in square brackets [1], [2], etc. Each citation number corresponds to the relevant context item in the order it was provided. \n
                 For example, if you're using information from the first context item, cite it as [1]. If you're using information from multiple context items, cite them as [1][2]. At the end of your answer, you must also add a 'Reference' section to provide the 'file_path' of the context you have cited.  \n
